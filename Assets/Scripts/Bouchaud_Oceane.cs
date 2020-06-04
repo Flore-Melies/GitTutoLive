7c533531059e64a9c09643f6cd179f0d0c8158b1
Bouchaud <o.bouchaud@etudiant.ecolemelies.fr>
2020-06-04 17:39:28 +0200

    Bouchaud_Oceane Script created;

---
+++ Assets/Scripts/Bouchaud_Oceane.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouchaud_Oceane : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
